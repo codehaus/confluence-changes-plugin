b0063cb5369ecc589feacdfd4c9af2cd10a5c259
bwalding <bwalding@71dc46fc-6f09-0410-bc8c-a980849e31bb>
2007-03-18 11:26:54 +0000

    Fix the delegator

--- confluence-changes-plugin/src/main/java/org/codehaus/confluence/changes/ChangesRPCImpl.java
+++ confluence-changes-plugin/src/main/java/org/codehaus/confluence/changes/ChangesRPCImpl.java
@@ -1,146 +1,34 @@
 package org.codehaus.confluence.changes;
 
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Vector;
 
-import com.atlassian.confluence.core.ContentEntityManager;
-import com.atlassian.confluence.core.ContentEntityObject;
-import com.atlassian.confluence.pages.BlogPost;
-import com.atlassian.confluence.pages.Comment;
-import com.atlassian.confluence.pages.Page;
-import com.atlassian.confluence.rpc.SecureRpc;
-import com.atlassian.confluence.security.Permission;
-import com.atlassian.confluence.security.PermissionManager;
-import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
-import com.atlassian.confluence.user.PersonalInformation;
-import com.atlassian.user.User;
 
-public class ChangesRPCImpl implements ChangesRPC, SecureRpc
+
+public class ChangesRPCImpl
+    implements ChangesRPC
 {
 
-    public String login( String username, String password )
+    public ChangesRPCImpl()
     {
-        //No implementation required; intercepted by Confluence
-        return null;
     }
 
-    public boolean logout( String token )
+    public void setChangesRPCImplDelegator(ChangesRPC changesrpc)
     {
-        //No implementation required; intercepted by Confluence
-        return false;
+        changesRPCImplDelegator = changesrpc;
     }
 
-    private User getUser()
+    public List getChanges(String s, String s1)
+        throws Exception
     {
-        return AuthenticatedUserThreadLocal.getUser();
+        return changesRPCImplDelegator.getChanges(s, s1);
     }
 
-    private ContentEntityManager contentEntityManager;
-
-    private PermissionManager permissionManager;
-
-    public List getChanges( String token, String lastChecked ) throws Exception
+    public String getSystemTime(String s)
     {
-        try
-        {
-            Date lastCheckedDate = new Date( Long.parseLong( lastChecked ) );
-
-            List results = new Vector();
-            List entitiesModifiedSince = contentEntityManager.getEntitiesModifiedSince( lastCheckedDate );
-
-            for ( Iterator iter = entitiesModifiedSince.iterator(); iter.hasNext(); )
-            {
-                ContentEntityObject ce = ( ContentEntityObject ) iter.next();
-                if ( !permissionManager.hasPermission( getUser(), Permission.VIEW, ce ) )
-                {
-                    continue;
-                }
-
-                Map map = new Hashtable();
-                results.add( map );
-
-                map.put( "id", convertLong( ce.getId() ) );
-                map.put( "lastModificationDate", convertDate( ce.getLastModificationDate() ) );
-                map.put( "lastModifierName", convertString( ce.getLastModifierName() ) );
-                map.put( "realTitle", convertString( ce.getRealTitle() ) );
-
-                if ( ce instanceof PersonalInformation )
-                {
-                    PersonalInformation o = ( PersonalInformation ) ce;
-                    map.put( "type", "PersonalInformation" );
-                    continue;
-                }
-
-                if ( ce instanceof Page )
-                {
-                    Page page = ( Page ) ce;
-                    map.put( "type", "Page" );
-                    continue;
-                }
-
-                if ( ce instanceof Comment )
-                {
-                    Comment comment = ( Comment ) ce;
-                    map.put( "type", "Comment" );
-                    continue;
-                }
-
-                if ( ce instanceof BlogPost )
-                {
-                    BlogPost blogPost = ( BlogPost ) ce;
-                    map.put( "type", "BlogPost" );
-                    continue;
-                }
-
-                //Fall back            
-                map.put( "className", ce.getClass().getName() );
-                results.add( map );
-            }
-            return results;
-        }
-        catch ( Exception e )
-        {
-            e.printStackTrace();
-            throw e;
-        }
+        return changesRPCImplDelegator.getSystemTime(s);
     }
 
-    private Object convertString( String s )
-    {
-        return s == null ? "" : s;
-    }
-
-    private String convertLong( long id )
-    {
-        return Long.toString( id );
-    }
-
-    private String convertDate( Date date )
-    {
-        if ( date == null )
-        {
-            return "";
-        }
-
-        return Long.toString( date.getTime() );
-    }
-
-    public void setContentEntityManager( ContentEntityManager contentEntityManager )
-    {
-        this.contentEntityManager = contentEntityManager;
-    }
-
-    public void setPermissionManager( PermissionManager permissionManager )
-    {
-        this.permissionManager = permissionManager;
-    }
-
-    public String getSystemTime( String token )
-    {
-        return Long.toString( System.currentTimeMillis() );
-    }
+    private ChangesRPC changesRPCImplDelegator;
 }
+
+
